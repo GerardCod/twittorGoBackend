baeae53414a741d66d2926f03fec9f7fe264aafb
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2020-09-16 21:06:33 -0500

    Accessing to DB

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/GerardCod/twittorGoBackend/bd"
+	"github.com/GerardCod/twittorGoBackend/handlers"
+)
+
+func main() {
+	if bd.CheckConnection() == 0 {
+		log.Fatal("No connection.")
+		return
+	}
+	handlers.Handlers()
 }
